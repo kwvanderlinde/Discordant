a686ea81651179464f5f49ed50cc5eaf66661fdb
Kenneth VanderLinde <kwvanderlinde@gmail.com>
2022-10-11 18:08:48 -0700

    Add free-form avatar URLs that can be embedded into messages
    The avatars are specifies as keys in the `"avatarUrls"` object and the values function just like the player icon
    URL. Messages can reference any of these by using the `{player.avatarUrls|<key>}` in addition to being able to use
    `{player.iconUrl}`.
    
    The reason for keeping `{player.iconUrl}` is that we want a flat face always available for use as author icons in
    Discord messages.
    
    The default URLs for player icons and avatars has been changed from cravatar.eu to crafatar.com. Acknowledgement has
    been added to the default channel topic.
    
    The generated `config.json` has been tweaked to no longer do HTML escaping as that just adds noise.

--- src/main/java/com/kwvanderlinde/discordant/core/config/DiscordMessagesConfig.java
+++ src/main/java/com/kwvanderlinde/discordant/core/config/DiscordMessagesConfig.java
@@ -9,12 +9,12 @@ import com.kwvanderlinde.discordant.core.messages.scopes.ServerScope;
 import java.awt.Color;
 
 public class DiscordMessagesConfig {
-    public DiscordMessageConfig<ServerScope> serverStart = new DiscordMessageConfig<>("{server.name} started", new Color(0x00ff00), null, "> _{server.motd}_");
+    public DiscordMessageConfig<ServerScope> serverStart = new DiscordMessageConfig<>("{server.name} started", new Color(0x00ff00), null, "_{server.motd}_");
     public DiscordMessageConfig<ServerScope> serverStop = new DiscordMessageConfig<>("{server.name} stopped", new Color(0xff0000), null, null);
-    public DiscordMessageConfig<PlayerScope> playerJoin = new DiscordMessageConfig<>("Player joined", new Color(0x00ff00), "{player.iconUrl}", "{username} joined the server!");
+    public DiscordMessageConfig<PlayerScope> playerJoin = new DiscordMessageConfig<>("Player joined", new Color(0x00ff00), "{player.avatarUrls|body}", "{username} joined the server!");
     public DiscordMessageConfig<PlayerScope> playerLeave = new DiscordMessageConfig<>("Player left", new Color(0xff0000), null, "{username} left the server!");
-    public DiscordMessageConfig<AdvancementScope> playerAdvancement = new DiscordMessageConfig<>("{player.name} has completed the challenge {advancement.title}", new Color(0xBF1AED), "{player.iconUrl}", "{advancement.description}");
-    public DiscordMessageConfig<DeathScope> playerDeath = new DiscordMessageConfig<>("The unthinkable has happened", new Color(0x000000), "{player.iconUrl}", "{death.message}");
+    public DiscordMessageConfig<AdvancementScope> playerAdvancement = new DiscordMessageConfig<>("{player.name} has completed the challenge {advancement.title}", new Color(0xBF1AED), "{player.avatarUrls|head}", "{advancement.description}");
+    public DiscordMessageConfig<DeathScope> playerDeath = new DiscordMessageConfig<>("The unthinkable has happened", new Color(0x000000), "{player.avatarUrls|head}", "{death.message}");
     public DiscordMessageConfig<ChatScope> playerChat = new DiscordMessageConfig<>(null, new Color(0x00ffff), null, "{chat.message}");
     public DiscordMessageConfig<PlayerScope> successfulVerification = new DiscordMessageConfig<>("Account linking successful!", new Color(0x92D22E), null, "Successfully linked discord account to your game account {username}({uuid})");
     public DiscordMessageConfig<PlayerScope> alreadyLinked = new DiscordMessageConfig<>("Account already linked", new Color(0x992D22), null, "Game account {username} is already linked to {discordname}");
--- src/main/java/com/kwvanderlinde/discordant/core/config/DiscordTopicsConfig.java
+++ src/main/java/com/kwvanderlinde/discordant/core/config/DiscordTopicsConfig.java
@@ -3,6 +3,6 @@ package com.kwvanderlinde.discordant.core.config;
 import com.kwvanderlinde.discordant.core.messages.scopes.ServerScope;
 
 public class DiscordTopicsConfig {
-    public TopicConfig<ServerScope> channelTopic = new TopicConfig<>("Players online: {server.playerCount} / {server.maxPlayerCount}");
+    public TopicConfig<ServerScope> channelTopic = new TopicConfig<>("Players online: {server.playerCount} / {server.maxPlayerCount} | Thank you to Crafatar for providing avatars: https://crafatar.com");
     public TopicConfig<ServerScope> shutdownTopic = new TopicConfig<>("Server offline");
 }
