0e758718ee7233aa76f4f7fdbdfa78afd039c932
Kenneth VanderLinde <kwvanderlinde@gmail.com>
2022-10-16 00:33:53 -0700

    Remove ConfigurationDependentServices

--- src/main/java/com/kwvanderlinde/discordant/core/ConfigDependantServices.java
+++
@@ -1,9 +0,0 @@
-package com.kwvanderlinde.discordant.core;
-
-import com.kwvanderlinde.discordant.core.discord.api.ReplaceableDiscordApi;
-import com.kwvanderlinde.discordant.core.linkedprofiles.LinkedProfileManager;
-
-public record ConfigDependantServices(ReplaceableDiscordApi discordApi,
-                                      LinkedProfileManager linkedProfileManager,
-                                      ScopeFactory scopeFactory) {
-}
