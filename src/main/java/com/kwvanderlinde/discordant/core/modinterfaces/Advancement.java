a6a1c069cc75e743036379457024a613f739f476
Kenneth VanderLinde <kwvanderlinde@gmail.com>
2024-01-30 08:28:49 -0800

    Only send advancment embeds for advancements announced to chat

--- src/main/java/com/kwvanderlinde/discordant/core/modinterfaces/Advancement.java
+++ src/main/java/com/kwvanderlinde/discordant/core/modinterfaces/Advancement.java
@@ -7,6 +7,6 @@ public record Advancement(Optional<Display> display) {
     public record Type(String name, @Nullable Integer rgb) {
     }
 
-    public record Display(String title, String description, Type type) {
+    public record Display(String title, String description, Type type, boolean announceToChat) {
     }
 }
